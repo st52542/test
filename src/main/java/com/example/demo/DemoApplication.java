e54217c978cbd1b9a62b5614d7e3bb9d9f940477
st52542 <43936282+st52542@users.noreply.github.com>
2020-12-03 13:58:24 +0100

    Revert "blabla"
    This reverts commit 604865c6

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("pokusdsasasda");
-		SpringApplication.run(DemoApplication, args);
+		System.out.println("pokus");
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
